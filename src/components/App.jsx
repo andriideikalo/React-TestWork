b9bb1b91965edb62e89c250d34361afb55c6d55a
andriideikalo <wlada801@gmail.com>
2023-01-09 12:01:22 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,11 @@
 import React from 'react';
 
-// import { HandlerClickPlus } from './JS/button';
-import { HandlerClickMinus } from './JS/button';
-import { BbuttonStyles } from './JS/botton.Styles';
-import { ContainerStyles } from './JS/containerStyles';
-import { render } from '@testing-library/react';
+import ClickPlus from './JS/button';
 
-class ClickPlus extends React.Component {
-  classClickPlus = () => {
-    console.log('press plus');
-    console.log(this);
-  };
-
-  render() {
-    return (
-      <ContainerStyles className="ClickPlus">
-        <BbuttonStyles
-          type="button"
-          // onClick={() => {
-          // HandlerClickPlus();
-          // }}
-          onClick={this.classClickPlus}
-        >
-          + Click
-        </BbuttonStyles>
-        <BbuttonStyles
-          type="button"
-          onClick={() => {
-            HandlerClickMinus();
-          }}
-        >
-          - Click
-        </BbuttonStyles>
-      </ContainerStyles>
-    );
-    // };
-  }
-}
+const App = () => (
+  <>
+    <h1>Стан компоненту</h1>
+    <ClickPlus />
+  </>
+);
+export default App;
--- src/components/JS/button.js
+++ src/components/JS/button.js
@@ -1,7 +1,42 @@
-export const HandlerClickPlus = () => {
-  console.log('press plus');
-};
+import React from 'react';
 
-export const HandlerClickMinus = () => {
-  console.log('press minus');
-};
+// import { HandlerClickPlus } from './JS/button';
+import { HandlerClickMinus } from './JS/button';
+import { BbuttonStyles } from './JS/botton.Styles';
+import { ContainerStyles } from './JS/containerStyles';
+// import { render } from '@testing-library/react';
+
+class ClickPlus extends React.Component {
+  render() {
+    return (
+      <ContainerStyles className="ClickPlus">
+        <BbuttonStyles
+          type="button"
+          // onClick={() => {
+          // HandlerClickPlus();
+          // }}
+          onClick={this.classClickPlus}
+        >
+          + Click
+        </BbuttonStyles>
+        <BbuttonStyles
+          type="button"
+          onClick={() => {
+            HandlerClickMinus();
+          }}
+        >
+          - Click
+        </BbuttonStyles>
+      </ContainerStyles>
+    );
+    // };
+  }
+}
+
+// export const HandlerClickPlus = () => {
+//   console.log('press plus');
+// };
+
+// export const HandlerClickMinus = () => {
+//   console.log('press minus');
+// };
